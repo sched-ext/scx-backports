ff6ba43ebc6a8b816e5aedc21bd39a7ec7da4469
Pat Somaru <patso@likewhatevs.io>
2024-11-08 21:40:23 -0500

    setup backports repo
    add helper scripts etc.

---
+++ backport-scripts/exec-entrypoint.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+cd /
+
+if [ -f /.dockerenv ]; then
+    echo "Running artifact build in docker container";
+else
+    echo "This script is not being ran inside a docker container, please run this script inside a docker container.";
+fi
+
+git clone --recurse-submodules https://github.com/libbpf/bpftool.git /bpftool
+
+cd /bpftool/src
+
+make -j "$(nproc)"
+
+make -j "$(nproc)" install
+
+cd /
+
+SHORT_SHA="$1"
+BRANCH="$2"
+REPO="$3"
+
+git clone -b "$BRANCH" "$REPO" /sched-ext-linux
+
+cd /sched-ext-linux
+
+git checkout "$SHORT_SHA"
+
+# this is for backports and mixing new bpf with old kernel
+find . -type f -exec sed -i 's/-Werror/-Wno-error/g' {} \;
+
+vng -v --kconfig --config /sched-ext.config
+
+make -j "$(nproc)"
+
+bpftool btf dump file "/sched-ext-linux/vmlinux" format c > "/vmlinux-${SHORT_SHA}.h"
+
+echo "generated vmlinux-${SHORT_SHA}.h"
