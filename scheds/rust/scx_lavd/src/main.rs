694c5109df3e4977956771f83db40950f5643c18
Pat Somaru <patso@likewhatevs.io>
2024-12-08 13:04:38 -0500

    remove build_id uses
    (cherry picked from commit b76d1429c786278c13f38d92dc62be4bc40e207d)
    (cherry picked from commit 57cc4ed9ec009f073bc2fbb35da43ed1137f8698)
    (cherry picked from commit 75ce62f4d2074fc0e461ddbea7708dce8f882e4a)
    (cherry picked from commit 28ffbdf1eedc806a7ff4d831d69c254d64a5ebe3)
    (cherry picked from commit ebe977cebfa52a6cbb6372af806643c653dc0fc7)
    (cherry picked from commit ead9589d363476a4d5e5ac71b0abaf1b3ff9a4a1)
    (cherry picked from commit fcba6593c47504bf2ea2fd48e55602820bef9a63)

--- scheds/rust/scx_lavd/src/main.rs
+++ scheds/rust/scx_lavd/src/main.rs
@@ -49,8 +49,7 @@ use log::info;
 use plain::Plain;
 use scx_stats::prelude::*;
 use scx_utils::autopower::{fetch_power_profile, PowerProfile};
-use scx_utils::build_id;
-use scx_utils::compat;
+// use scx_utils::build_id;
 use scx_utils::import_enums;
 use scx_utils::scx_enums;
 use scx_utils::scx_ops_attach;
@@ -929,10 +928,7 @@ fn main() -> Result<()> {
     let mut opts = Opts::parse();
 
     if opts.version {
-        println!(
-            "scx_lavd {}",
-            build_id::full_version(env!("CARGO_PKG_VERSION"))
-        );
+        // println!("scx_lavd {}", *build_id::SCX_FULL_VERSION);
         return Ok(());
     }
 
@@ -982,10 +978,10 @@ fn main() -> Result<()> {
     let mut open_object = MaybeUninit::uninit();
     loop {
         let mut sched = Scheduler::init(&opts, &mut open_object)?;
-        info!(
-            "scx_lavd scheduler is initialized (build ID: {})",
-            build_id::full_version(env!("CARGO_PKG_VERSION"))
-        );
+        // info!(
+        //     "scx_lavd scheduler is initialized (build ID: {})",
+        //     *build_id::SCX_FULL_VERSION
+        // );
         info!("scx_lavd scheduler starts running.");
         if !sched.run(&opts, shutdown.clone())?.should_restart() {
             break;
