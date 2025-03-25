e79c2be156117fdebe0196fb7a36eb596f96e465
Pat Somaru <patso@likewhatevs.io>
2025-03-25 15:12:19 -0400

    cleanup rebase

--- scheds/rust/scx_lavd/src/main.rs
+++ scheds/rust/scx_lavd/src/main.rs
@@ -44,7 +44,7 @@ use log::info;
 use plain::Plain;
 use scx_stats::prelude::*;
 use scx_utils::autopower::{fetch_power_profile, PowerProfile};
-use scx_utils::build_id;
+//use scx_utils::build_id;
 use scx_utils::compat;
 use scx_utils::scx_ops_attach;
 use scx_utils::scx_ops_load;
